3368ae120f0520bd44f0d1667084c53c63f3163a
alanfelp <alan.felipe@querodelivery.com>
2020-08-19 18:29:41 -0300

    add modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import InputLine from "./components/InputLine/InputLine";
 import ButtonMain from "./components/ButtonMain/ButtonMain";
 import Loader from "./components/Loader/Loader";
+import Modal from "./components/Modal/Modal";
 
-export { InputLine, ButtonMain, Loader };
+export { InputLine, ButtonMain, Loader, Modal };
