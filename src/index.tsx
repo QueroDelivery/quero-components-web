1c12226c4ddcb592278f0783277bdef7b323bd0b
alanfelp <alan.felipe@querodelivery.com>
2020-08-26 18:23:02 -0300

    remove toast

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,5 @@ import ButtonMain from "./components/ButtonMain/ButtonMain";
 import Loader from "./components/Loader/Loader";
 import Modal from "./components/Modal/Modal";
 import Dialog from "./components/Dialog/Dialog";
-import { ToastProvider, showToast } from "./hooks/Toast";
 
-export {
-    InputLine,
-    ButtonMain,
-    Loader,
-    Modal,
-    Dialog,
-    ToastProvider,
-    showToast,
-};
+export { InputLine, ButtonMain, Loader, Modal, Dialog };
