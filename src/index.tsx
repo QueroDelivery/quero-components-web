b639124ef5065c1d24ce3dfddfbd3ff9258f695d
alanfelp <alan.felipe@querodelivery.com>
2020-09-02 11:18:39 -0300

    add text area

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import Dialog from '../src/components/Dialog/Dialog';
 import InputLine from '../src/components/InputLine/InputLine';
 import Loader from '../src/components/Loader/Loader';
 import Modal from '../src/components/Modal/Modal';
+import TextArea from '../src/components/TextArea/TextArea';
 
-export { ButtonMain, Dialog, InputLine, Loader, Modal };
+export { ButtonMain, Dialog, InputLine, Loader, Modal, TextArea };
