e194eea1f021c719a824fe7ca23e751e924f1cd7
alanflp <alan.felipe@querodelivery.com>
2021-02-15 08:43:47 -0300

    remove dist

--- dist/components/Dialog/Dialog.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export interface DialogProps {
-    open: boolean;
-    onClose: Function;
-    loading?: boolean;
-    maxHeight?: number;
-}
-declare const DialogComponent: React.FC<DialogProps>;
-export default DialogComponent;
--- dist/helpers/FnUtil.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const removerAcentos: (inStr: string) => string;
-export declare const removerAcentosComApostofro: (inStr: string) => string;
-export declare const removerAcentosESpecialChars: (str: string) => string;
-export declare const replaceSpecialChars: (str: string) => string;
