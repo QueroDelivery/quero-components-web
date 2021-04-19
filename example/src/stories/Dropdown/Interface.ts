4f4feeef4660a548c448c309a92bbcfe9c2f6978
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-04-19 13:31:22 -0300

    docs: adjusts interface Dropdown

--- example/src/stories/Dropdown/Interface.ts
+++ example/src/stories/Dropdown/Interface.ts
@@ -6,22 +6,6 @@ interface OptionsProps {
     value: any;
 }
 
-// export interface DropdownProps {
-//     brand?: boolean;
-//     options: OptionsProps[];
-//     value: any;
-//     onChange: (value: any) => void;
-//     placeholder?: string;
-//     textAlign?: "center";
-//     search?: boolean;
-//     loading?: boolean;
-//     width?: number;
-//     errorMessage?: string;
-//     label?: string;
-//     labelColor?: string;
-//     dropdownRef?: any;
-//     height?: number;
-// }
 export interface Props extends DropdownProps {
     options: OptionsProps[];
     brand: boolean;
@@ -29,4 +13,5 @@ export interface Props extends DropdownProps {
     errorMessage: string;
     label: string;
     line: boolean;
+    colorLabel?: string;
 }
