6ef3197323d2013b07b5bc577138d49421071d57
alanflp <alan.felipe@querodelivery.com>
2021-05-03 15:45:12 -0300

    publish

--- example/storybook-static/service-worker.js
+++ example/storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3b482031a0aefaf217d662d1da07f029.js"
+  "precache-manifest.30004bbdd7b8225672c802dda5651ebb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
-
+  
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
