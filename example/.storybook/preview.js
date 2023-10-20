57f756bb16af866a911543b85017a59036e94c76
Edvan de Matos <edvan.santos@querodelivery.com>
2023-10-20 14:20:54 -0300

    docs: :memo: adjusting docs

--- example/.storybook/preview.js
+++ example/.storybook/preview.js
@@ -1,9 +1,33 @@
+import React from 'react';
+
+import { StyleSheetManager } from 'styled-components';
+import isPropValid from '@emotion/is-prop-valid';
+
+// This implements the default behavior from styled-components v5
+function shouldForwardProp(propName, target) {
+  if (typeof target === 'string') {
+    // For HTML elements, forward the prop if it is a valid HTML attribute
+    return isPropValid(propName);
+  }
+  // For other elements, forward all props
+  return true;
+}
+
+export const decorators = [
+  Story => {
+    return (
+      <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+        <Story />
+      </StyleSheetManager>
+    );
+  },
+];
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
