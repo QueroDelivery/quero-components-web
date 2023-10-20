d9a76b3fe57adf34caa24123ffaecbf57b0924a1
Edvan de Matos <edvan.santos@querodelivery.com>
2023-10-20 13:21:40 -0300

    feat: :construction_worker: improve library structure

--- dist/esm/components/Calendar/styles.d.ts
+++
@@ -1,7 +0,0 @@
-import { sizesTypes } from '../../helpers/FnUtil';
-interface CalendarProps {
-    brand?: boolean;
-    size?: sizesTypes;
-}
-export declare const Container: import("styled-components").StyledComponent<"div", any, CalendarProps, never>;
-export {};
