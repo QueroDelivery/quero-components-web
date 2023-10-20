ea1969201547748d5f1c789ff8f4ee3f64d3cc13
Edvan de Matos <edvan.santos@querodelivery.com>
2023-10-20 15:38:02 -0300

    build: :construction_worker: adjust styled-components dep

--- example/.storybook/preview.js
+++ example/.storybook/preview.js
@@ -1,27 +1,3 @@
-import React from 'react';
-
-import { StyleSheetManager } from 'styled-components';
-import isPropValid from '@emotion/is-prop-valid';
-
-// This implements the default behavior from styled-components v5
-function shouldForwardProp(propName, target) {
-  if (typeof target === 'string') {
-    // For HTML elements, forward the prop if it is a valid HTML attribute
-    return isPropValid(propName);
-  }
-  // For other elements, forward all props
-  return true;
-}
-
-export const decorators = [
-  Story => {
-    return (
-      <StyleSheetManager shouldForwardProp={shouldForwardProp}>
-        <Story />
-      </StyleSheetManager>
-    );
-  },
-];
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
