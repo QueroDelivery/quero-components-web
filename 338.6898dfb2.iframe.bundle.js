1204d0497fcea1189ce24b0699f2c9a19aa26b67
matos-ed <matos-ed@users.noreply.github.com>
2026-06-16 14:02:35 +0000

    deploy: 54f9668271d78595a65b6176fea904e7bb0e5715

---
+++ 338.6898dfb2.iframe.bundle.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_querodelivery_quero_components_web_example=self.webpackChunk_querodelivery_quero_components_web_example||[]).push([[338],{"./node_modules/react-dom/client.js"(__unused_webpack_module,exports,__webpack_require__){var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
+//# sourceMappingURL=338.6898dfb2.iframe.bundle.js.map
